293e5abe7cc215926c3d1c7a3577e36b7c95aee2
selimssevgi <selimssevgi@gmail.com>
2019-06-14 21:58:30 +0300

    feat: empty spring boot project

---
+++ src/main/java/com/cdt/be/CdtBeApplication.java
@@ -0,0 +1,13 @@
+package com.cdt.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdtBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CdtBeApplication.class, args);
+  }
+
+}
