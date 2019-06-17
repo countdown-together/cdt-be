9c081a4abee068db71112b6683788a3e6c7fef50
selimssevgi <selimssevgi@gmail.com>
2019-06-17 23:38:30 +0300

    feat: pull in actuator for helping endpoints

---
+++ src/test/java/com/cdt/be/infrastructure/actuator/ActuatorIntTest.java
@@ -0,0 +1,30 @@
+package com.cdt.be.infrastructure.actuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author selimssevgi
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ActuatorIntTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void providesHealthEndpoint() throws Exception {
+    mockMvc.perform(get("/actuator/health"))
+        .andExpect(status().isOk());
+  }
+}
